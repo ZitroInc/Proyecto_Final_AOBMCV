2f2a85a514b28eccf7a2c9415e5da09833e5b414
Andrés Ortiz <zitro.inc@gmail.com>
2023-12-13 01:02:32 -0300

    Reinicio de proyecto

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
+
+
 
 # Create your views here.
+
+def new_user(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)       """EDITABLES"""
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("")                     """REVISAR"""
+
+    form = UserRegisterForm()
+    contexto = {
+        "form": form
+    }
+    return render(request, "", contexto)    """REVISAR"""
