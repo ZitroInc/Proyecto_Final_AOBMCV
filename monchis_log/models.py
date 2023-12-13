002a1df36ba39c6e2ddcc0ebd5c3b1f58204828b
Bastian Conte <bmconte@gmail.com>
2023-12-13 01:42:04 -0300

    Prueba2

--- monchis_log/models.py
+++ monchis_log/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    nombre_local = models.CharField(max_length=100)
+    comentario = models.TextField()
+    imagen = models.ImageField(upload_to='uploads/')
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre_local
+
--- monchis_log/views.py
+++ monchis_log/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'mochis/index.html', {'posts': posts})
+
+def crear_posteo(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+    return render(request, 'mochis/crear_posteo.html', {'form': form})
+
+
