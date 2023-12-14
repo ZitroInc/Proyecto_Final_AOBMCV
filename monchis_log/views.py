4048b2ccefc655cbdff1c1b9788dfd5ce07957f9
Wverios <claudio.vega.r@gmail.com>
2023-12-14 00:49:16 -0300

    htmls

--- monchis_log/views.py
+++ monchis_log/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Post, Posteo_Lista
 from .forms import PostForm, BuscarForm
@@ -49,21 +50,44 @@ class PosteoLista(ListView):
 # def mostrar_posteos(request):
 
 
-#""" @login_required()"""
-# def crear_posteo_form(request):
-#     if request.method == "POST":
-#         post_formulario = PostForm(request.POST)
-#         if post_formulario.is_valid():
-#             informacion = post_formulario.cleaned_data
-#             post_crear = Post(nombre_local=informacion["nombre_local"], plato=informacion["plato"])
-#             crear_posteo_form.save()
-#             return redirect("##########cambiar!!!#########")
-#
-#     post_formulario = PostForm()
-#     contexto = {
-#         "form": post_formulario
-#     }
-#     return render(request, "monchis_log/crear_posteo.html", contexto)
+""" @login_required()"""
+def crear_posteo_form(request):
+    if request.method == "POST":
+        post_formulario = PostForm(request.POST)
+        if post_formulario.is_valid():
+            informacion = post_formulario.cleaned_data
+            crear_posteo = Post(nombre_local=informacion["nombre_local"], plato=informacion["plato"])
+            crear_posteo_form.save()
+            return redirect("monchis/posteos/")
 
+    post_formulario = PostForm()
+    contexto = {
+        "form": post_formulario
+    }
+    return render(request, "monchis/crear_posteo.html", contexto)
 
+class PostList(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = "monchis/posteos.html"
+
+class PostDetalle(LoginRequiredMixin, DetailView):
+    model = Post
+    template_name = "monchis/posteos_detalle.html"
+
+class PostCreacion(CreateView):
+    model = Post
+    sucess_url = xxxxxxxxxxx
+    template_name = "monchis/crear_posteo.html"
+    fields = ["nombre_local", "comentario"]
+
+class PostActualizacion(UpdateView):
+    model = Post
+    success_url = xxxxxxxxxxxxxxx
+    template_name = "monchis/crear_posteo.html"
+    fields = ["nombre_local", "comentario"]
+
+class PostEliminar(DeleteView):
+      model = Post
+      template_name = "monchis/eliminar_posteo.html"
+      success_url = "monchis/posteos/listar"
 
